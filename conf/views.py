d5f44347c67b1e70da05754e122f6d4a85942ec0
Andy Taylor <a.taylor@kainos.com>
2020-06-17 18:06:56 +0100

    Remove notify users, let pingdom handle this. Update validation to exlucde end user if not required

--- conf/views.py
+++ conf/views.py
@@ -2,13 +2,13 @@ import datetime
 import time
 
 from background_task.models import Task
-from django.shortcuts import render_to_response
+from django.http import HttpResponse
 from django.utils import timezone
 from rest_framework.status import HTTP_200_OK, HTTP_503_SERVICE_UNAVAILABLE
 from rest_framework.views import APIView
 
 from conf.settings import LITE_LICENCE_UPDATE_POLL_INTERVAL, INBOX_POLL_INTERVAL, EMAIL_AWAITING_REPLY_TIME
-from mail.enums import ReceptionStatusEnum
+from mail.enums import ReceptionStatusEnum, ReplyStatusEnum
 from mail.models import Mail
 from mail.tasks import LICENCE_UPDATES_TASK_QUEUE, MANAGE_INBOX_TASK_QUEUE
 
@@ -27,14 +27,21 @@ class HealthCheck(APIView):
             run_at__lte=timezone.now() + datetime.timedelta(seconds=LITE_LICENCE_UPDATE_POLL_INTERVAL),
         )
         if not licence_update_task.exists():
-            return self._build_response(HTTP_503_SERVICE_UNAVAILABLE, "not OK", start_time)
+            return self._build_response(self._get_response_time(start_time), "not OK", HTTP_503_SERVICE_UNAVAILABLE)
 
         # If no inbox task is scheduled to run in the next INBOX_POLL_INTERVAL seconds
         manage_inbox_task = Task.objects.filter(
             queue=MANAGE_INBOX_TASK_QUEUE, run_at__lte=timezone.now() + datetime.timedelta(seconds=INBOX_POLL_INTERVAL)
         )
         if not manage_inbox_task.exists():
-            return self._build_response(HTTP_503_SERVICE_UNAVAILABLE, "not OK", start_time)
+            return self._build_response(self._get_response_time(start_time), "not OK", HTTP_503_SERVICE_UNAVAILABLE)
+
+        # If an email has been rejected
+        rejected_email = Mail.objects.filter(
+            status=ReceptionStatusEnum.REPLY_SENT, response_data__icontains=ReplyStatusEnum.REJECTED,
+        )
+        if rejected_email.exists():
+            return self._build_response(self._get_response_time(start_time), "not OK", HTTP_503_SERVICE_UNAVAILABLE)
 
         # If an email has been awaiting for a reply for longer than EMAIL_AWAITING_REPLY_TIME seconds
         email_awaiting_response_for_prolonged_period_of_time = Mail.objects.filter(
@@ -42,13 +49,26 @@ class HealthCheck(APIView):
             sent_at__lte=timezone.now() - datetime.timedelta(seconds=EMAIL_AWAITING_REPLY_TIME),
         )
         if email_awaiting_response_for_prolonged_period_of_time.exists():
-            return self._build_response(HTTP_503_SERVICE_UNAVAILABLE, "not OK", start_time)
+            return self._build_response(self._get_response_time(start_time), "not OK", HTTP_503_SERVICE_UNAVAILABLE)
 
-        return self._build_response(HTTP_200_OK, "OK", start_time)
+        return self._build_response(self._get_response_time(start_time), "OK", HTTP_200_OK)
 
-    @staticmethod
-    def _build_response(status, message, start_time):
+    @classmethod
+    def _get_response_time(cls, start_time) -> str:
         duration_ms = (time.time() - start_time) * 1000
-        response_time = "{:.3f}".format(duration_ms)
-        context = {"message": message, "response_time": response_time}
-        return render_to_response("healthcheck.xml", context, content_type="application/xml", status=status)
+        return "{:.3f}".format(duration_ms)
+
+    @classmethod
+    def _get_xml(cls, status, response_time):
+        return f"""
+                   <pingdom_http_custom_check>
+                     <status>{status}</status> 
+                     <response_time>{response_time}</response_time>
+                   </pingdom_http_custom_check>
+                """
+
+    @classmethod
+    def _build_response(cls, response_time, message, status) -> HttpResponse:
+        return HttpResponse(
+            content=cls._get_xml(message, response_time), content_type="application/xml", status=status,
+        )
