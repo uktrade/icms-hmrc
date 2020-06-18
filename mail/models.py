b6342e849da5d5ccc51afd71865aec82506970e4
Andy Taylor <a.taylor@kainos.com>
2020-06-18 15:08:56 +0100

    hotfix

--- mail/models.py
+++ mail/models.py
@@ -61,7 +61,7 @@ class Mail(models.Model):
     def notify_users(id, response_date):
         from mail.tasks import notify_users_of_rejected_mail
 
-        notify_users_of_rejected_mail(id, response_date)
+        notify_users_of_rejected_mail(str(id), response_date)
 
 
 class LicenceUpdate(models.Model):
