e595131382481d7ac9677a5d6958de6f7cfc7c30
Kevin Carrogan <kevin.carrogan@gmail.com>
2022-08-31 13:36:02 +0100

    Correctly skip bandit violations on test password

--- mail/tests/libraries/test_routing_controller.py
+++ mail/tests/libraries/test_routing_controller.py
@@ -17,21 +17,21 @@ class RoutingControllerTest(unittest.TestCase):
         spec=ModernAuthentication,
     )
     @patch("mail.libraries.routing_controller.MailServer")
-    @override_settings(
+    @override_settings(  # nosec
         INCOMING_EMAIL_USER="incoming.email.user@example.com",
         INCOMING_EMAIL_HOSTNAME="host.example.com",
         INCOMING_EMAIL_POP3_PORT="123",
         AZURE_AUTH_CLIENT_ID="azure-auth-client-id",
-        AZURE_AUTH_CLIENT_SECRET="azure-auth-client-secret",  # nosec
+        AZURE_AUTH_CLIENT_SECRET="azure-auth-client-secret",
         AZURE_AUTH_TENANT_ID="azure-auth-tenant-id",
     )
     def test_get_spire_to_dit_mailserver(self, mock_MailServer, mock_ModernAuthentication):
         spire_to_dit_mailserver = get_spire_to_dit_mailserver()
 
-        mock_ModernAuthentication.assert_called_with(
+        mock_ModernAuthentication.assert_called_with(  # nosec
             user="incoming.email.user@example.com",
             client_id="azure-auth-client-id",
-            client_secret="azure-auth-client-secret",  # nosec
+            client_secret="azure-auth-client-secret",
             tenant_id="azure-auth-tenant-id",
         )
         mock_MailServer.assert_called_with(
@@ -47,21 +47,21 @@ class RoutingControllerTest(unittest.TestCase):
         spec=ModernAuthentication,
     )
     @patch("mail.libraries.routing_controller.MailServer")
-    @override_settings(
+    @override_settings(  # nosec
         HMRC_TO_DIT_EMAIL_USER="hmrc.to.dit.email.user@example.com",
         HMRC_TO_DIT_EMAIL_HOSTNAME="host.example.com",
         HMRC_TO_DIT_EMAIL_POP3_PORT="123",
         AZURE_AUTH_CLIENT_ID="azure-auth-client-id",
-        AZURE_AUTH_CLIENT_SECRET="azure-auth-client-secret",  # nosec
+        AZURE_AUTH_CLIENT_SECRET="azure-auth-client-secret",
         AZURE_AUTH_TENANT_ID="azure-auth-tenant-id",
     )
     def test_get_hmrc_to_dit_mailserver(self, mock_MailServer, mock_ModernAuthentication):
         hmrc_to_dit_mailserver = get_hmrc_to_dit_mailserver()
 
-        mock_ModernAuthentication.assert_called_with(
+        mock_ModernAuthentication.assert_called_with(  # nosec
             user="hmrc.to.dit.email.user@example.com",
             client_id="azure-auth-client-id",
-            client_secret="azure-auth-client-secret",  # nosec
+            client_secret="azure-auth-client-secret",
             tenant_id="azure-auth-tenant-id",
         )
         mock_MailServer.assert_called_with(
@@ -77,18 +77,18 @@ class RoutingControllerTest(unittest.TestCase):
         spec=BasicAuthentication,
     )
     @patch("mail.libraries.routing_controller.MailServer")
-    @override_settings(
+    @override_settings(  # nosec
         MOCK_HMRC_EMAIL_USER="mock.hmrc.email.user@example.com",
-        MOCK_HMRC_EMAIL_PASSWORD="shhh",  # nosec
+        MOCK_HMRC_EMAIL_PASSWORD="shhh",
         MOCK_HMRC_EMAIL_HOSTNAME="host.example.com",
         MOCK_HMRC_EMAIL_POP3_PORT="123",
     )
     def test_get_mock_hmrc_mailserver(self, mock_MailServer, mock_BasicAuthentication):
         mock_hmrc_mailserver = get_mock_hmrc_mailserver()
 
-        mock_BasicAuthentication.assert_called_with(
+        mock_BasicAuthentication.assert_called_with(  # nosec
             user="mock.hmrc.email.user@example.com",
-            password="shhh",  # nosec
+            password="shhh",
         )
         mock_MailServer.assert_called_with(
             mock_BasicAuthentication(),
