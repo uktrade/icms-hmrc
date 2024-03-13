d85a1b12057614e51ebb2ed73510dc4e4081f4f1
Lawrence Goldstien <lawrence.goldstein@digital.trade.gov.uk>
2024-03-13 09:34:05 +0000

    DPM-159 - DBT platform build configuration

---
+++ .copilot/image_build_run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Exit early if something goes wrong
+set -e
+
+# Add commands below to run inside the container after all the other buildpacks have been applied
+export BUILD_STEP='True'
+export COPILOT_ENVIRONMENT_NAME='build'
+
+python manage.py collectstatic --noinput --traceback
