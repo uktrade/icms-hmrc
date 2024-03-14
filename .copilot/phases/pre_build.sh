c0edc490084d733a1f3c653411d498d7d674159a
Lawrence Goldstien <lawrence.goldstein@digital.trade.gov.uk>
2024-03-14 08:53:11 +0000

    DPM-159 - Use pipenv to generate requirements.txt

--- .copilot/phases/pre_build.sh
+++ .copilot/phases/pre_build.sh
@@ -6,5 +6,5 @@ set -e
 # Add commands below to run as part of the pre_build phase
 
 # Pipfile is not supported as an installation method
-jq -r '.default | to_entries[] | .key + .value.version' Pipfile.lock > requirements.txt
+pipenv requirements > requirements.txt
 rm -f Pipfile Pipfile.lock
